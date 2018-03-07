e040ca12d54ba68d0ca4d901541293b57fe17b31
fiore nu <fiorella.nucara@gmail.com>
2018-03-06 23:43:28 -0300

    Todas las pantallas, falta limpiar el codigo

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function mostrarDiv() {
+	var divOculto = document.getElementById('idoculto');
+	divOculto.style.display = "block";
+}
+
+function ocultarDiv() {
+	var divOculto = document.getElementById('idoculto');
+	divOculto.style.display = "none";
+}
